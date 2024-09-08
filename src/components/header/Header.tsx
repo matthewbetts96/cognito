41fef14085d67f6e18ba11274ea4d2be7096e69e
matthew.betts.96@gmail.com <matthew.betts.96@gmail.com>
2024-09-08 14:05:51 +0100

    Add routing to top menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,29 @@
 import styled from "styled-components";
 import image from "assets/kiwk-e-mart.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { MenuItem, Typography } from "@mui/material";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const pages = [
+    { name: "Products", route: "/" },
+    { name: "Basket", route: "/basket" },
+  ];
+
   return (
     <Wrapper>
       <Link to={`/`}>
         <img alt={"kwik-e-mart-logo"} src={image} />
       </Link>
+
+      {pages.map((i) => {
+        return (
+          <MenuItem key={i.name} onClick={() => navigate(i.route)}>
+            <Typography sx={{ textAlign: "center" }}>{i.name}</Typography>
+          </MenuItem>
+        );
+      })}
     </Wrapper>
   );
 };
@@ -20,6 +36,7 @@ const Wrapper = styled.div`
   background: red;
   z-index: 10;
   align-content: center;
+  display: flex;
 `;
 
 export default Header;
